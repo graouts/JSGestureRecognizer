aa57418f21545f3c1a6e80bc8337cdbf0dffa9b0
Antoine Quint <antoine@graougraou.com>
2014-03-05 12:59:07 +0100

    Bring our pan gesture recognizer closer to the feature of UIPanGestureRecognizer by making .translate accumulative and reporting an accurate velocity by storing gesture information over the last second or since the last change of pan direction.

--- pan.js
+++ pan.js
@@ -43,19 +43,69 @@ PanGestureRecognizer.prototype = {
 
         event.preventDefault();
 
-        if (!this._beganRecognizer) {
+        var p = Point.fromEvent(event);
+        var currentTime = event.timeStamp;
+        this._recordGesture(p, currentTime);
+
+        if (this._gestures.length === 1) {
             this.enterBeganState();
-            this._beganRecognizer = true;
-            this._translationOrigin = Point.fromEvent(event);
+            this._translationOrigin = p;
         } else {
             this.enterChangedState();
-            var p = Point.fromEvent(event);
+
+            var sliceIndexX = 0;
+            var sliceIndexY = 0;
+
+            var currentGesture = this._gestures[this._gestures.length - 1];
+            var previousGesture = this._gestures[this._gestures.length - 2];
+
+            var txDirection = currentGesture.translation.x >= previousGesture.translation.x;
+            var tyDirection = currentGesture.translation.y >= previousGesture.translation.y;
+            for (var i = this._gestures.length - 3; i >= 0; --i) {
+                var gesture = this._gestures[i];
+                if (currentTime - gesture.timeStamp) {
+                    if (sliceIndexX === 0)
+                        sliceIndexX = i + 1;
+                    if (sliceIndexY === 0)
+                        sliceIndexY = i + 1;
+                    break;
+                }
+                
+                var nextTranslation = this._gestures[i + 1].translation;
+                
+                if (nextTranslation.x >= gesture.translation.x !== txDirection)
+                    sliceIndexX = i + 1;
+                if (nextTranslation.y >= gesture.translation.y !== tyDirection)
+                    sliceIndexY = i + 1;
+                
+                if (sliceIndexX > 0 && sliceIndexY > 0) 
+                    break;
+            }
+
+            var xGestures = this._gestures,
+                yGestures = this._gestures;
+
+            if (sliceIndexX === sliceIndexY && sliceIndexX > 0)
+                this._gestures = this._gestures.slice(sliceIndexX);
+            else {
+                if (sliceIndexX > 0)
+                    xGestures = this._gestures.slice(sliceIndexX);
+                if (sliceIndexY > 0)
+                    yGestures = this._gestures.slice(sliceIndexY);
+            } 
+
+            var oldestGestureX = xGestures[0];
+            var xdt = currentTime - oldestGestureX.timeStamp;
+            var dx = p.x - oldestGestureX.translation.x;
+            this.velocity.x = dx / xdt * 1000;
+
+            var oldestGestureY = yGestures[0];
+            var ydt = currentTime - oldestGestureY.timeStamp;
+            var dy = p.y - oldestGestureY.translation.y;
+            this.velocity.y = dy / ydt * 1000;
             
-            this.velocity.x = p.x - this.translation.x;
-            this.velocity.y = p.y - this.translation.y;
-            
-            this.translation.x = p.x - this._translationOrigin.x;
-            this.translation.y = p.y - this._translationOrigin.y;
+            this.translation.x += p.x - previousGesture.translation.x;
+            this.translation.y += p.y - previousGesture.translation.y;
         }
     },
     
@@ -72,8 +122,18 @@ PanGestureRecognizer.prototype = {
     
     reset: function()
     {
-        this._beganRecognizer = false;
+        this._gestures = [];
         this.translation = new Point;
         this.velocity = new Point;
+    },
+    
+    // Private
+    
+    _recordGesture: function(translation, timeStamp)
+    {
+        this._gestures.push({
+            translation: translation,
+            timeStamp: timeStamp
+        });
     }
 };
