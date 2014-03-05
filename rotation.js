42c460982d8064fb57389f3bfe4604ad9924d67f
Antoine Quint <antoine@graougraou.com>
2014-03-05 12:11:28 +0100

    Bring our rotation gesture recognizer closer to the feature of UIRotationGestureRecognizer by making .rotation accumulative and reporting an accurate velocity by storing gesture information over the last second or since the last change of rotation direction.

--- rotation.js
+++ rotation.js
@@ -21,26 +21,63 @@ RotationGestureRecognizer.prototype = {
         GestureRecognizer.prototype.touchesBegan.call(this, event);
     },
     
+    gestureBegan: function(event)
+    {
+        GestureRecognizer.prototype.gestureBegan.call(this, event);
+
+        this._recordGesture(event);
+        this.rotation = event.rotation;
+        this.enterBeganState();
+    },
+    
     gestureChanged: function(event)
     {
         if (event.target !== this.target)
             return;
 
         event.preventDefault();
-        if (this.beganRecognizer == false) {
-            this.enterBeganState();
-            this.beganRecognizer = true;
-        } else {
-            this.enterChangedState();
-            this.velocity = event.rotation - this.rotation;
-            this.rotation = event.rotation;
-        }
+
+        this.enterChangedState();
+
+        this._recordGesture(event);
+
+        var oldestGesture = this._gestures[0];
+        var dr = event.rotation - oldestGesture.rotation;
+        var dt = event.timeStamp - oldestGesture.timeStamp;
+        this.velocity = dr / dt * 1000;
+
+        this.rotation += event.rotation - this._gestures[this._gestures.length - 2].rotation;
     },
     
     reset: function()
     {
-        this.beganRecognizer = false;
         this.rotation = 0;
         this.velocity = 0;
+        this._gestures = [];
+    },
+    
+    // Private
+    
+    _recordGesture: function(event)
+    {
+        var currentTime = event.timeStamp;
+        var count = this._gestures.push({
+            rotation: event.rotation,
+            timeStamp: currentTime
+        });
+
+        // We want to keep at least two gestures at all times.
+        if (count <= 2)
+            return;
+
+        var rotationDirection = this._gestures[count - 1].rotation >= this._gestures[count - 2].rotation;
+        for (var i = count - 3; i >= 0; --i) {
+            var gesture = this._gestures[i];
+            if (currentTime - gesture.timeStamp > 1000 || this._gestures[i + 1].rotation >= gesture.rotation !== rotationDirection)
+                break;
+        }
+
+        if (i > 0)
+            this._gestures = this._gestures.splice(i + 1);
     }
 };
