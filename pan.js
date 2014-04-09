7b9e40aa59707d5d4d23b5411d4f9a1e6ee272c8
Antoine Quint <antoine@graougraou.com>
2014-04-09 15:39:00 +0200

    Ensure the velocity of a pan gesture is accurate even outside of the "changed" state.

--- pan.js
+++ pan.js
@@ -59,67 +59,70 @@ PanGestureRecognizer.prototype = {
         if (this._gestures.length === 1) {
             this._translationOrigin = location;
             this._travelledMinimumDistance = false;
-        } else if (!this._travelledMinimumDistance) {
+            return;
+        }
+
+        var sliceIndexX = 0;
+        var sliceIndexY = 0;
+
+        var currentGesture = this._gestures[this._gestures.length - 1];
+        var previousGesture = this._gestures[this._gestures.length - 2];
+
+        var txDirection = currentGesture.location.x >= previousGesture.location.x;
+        var tyDirection = currentGesture.location.y >= previousGesture.location.y;
+        for (var i = this._gestures.length - 3; i >= 0; --i) {
+            var gesture = this._gestures[i];
+            if (currentTime - gesture.timeStamp > PanGestureRecognizer.MaximumTimeForRecordingGestures) {
+                if (sliceIndexX === 0)
+                    sliceIndexX = i + 1;
+                if (sliceIndexY === 0)
+                    sliceIndexY = i + 1;
+                break;
+            }
+            
+            var nextTranslation = this._gestures[i + 1].location;
+            
+            if (nextTranslation.x >= gesture.location.x !== txDirection)
+                sliceIndexX = i + 1;
+            if (nextTranslation.y >= gesture.location.y !== tyDirection)
+                sliceIndexY = i + 1;
+            
+            if (sliceIndexX > 0 && sliceIndexY > 0) 
+                break;
+        }
+
+        var xGestures = this._gestures,
+            yGestures = this._gestures;
+
+        if (sliceIndexX === sliceIndexY && sliceIndexX > 0)
+            this._gestures = this._gestures.slice(sliceIndexX);
+        else {
+            if (sliceIndexX > 0)
+                xGestures = this._gestures.slice(sliceIndexX);
+            if (sliceIndexY > 0)
+                yGestures = this._gestures.slice(sliceIndexY);
+        } 
+
+        var oldestGestureX = xGestures[0];
+        var xdt = currentTime - oldestGestureX.timeStamp;
+        var dx = location.x - oldestGestureX.location.x;
+        this._velocity.x = dx / xdt * 1000;
+
+        var oldestGestureY = yGestures[0];
+        var ydt = currentTime - oldestGestureY.timeStamp;
+        var dy = location.y - oldestGestureY.location.y;
+        this._velocity.y = dy / ydt * 1000;
+
+        if (!this._travelledMinimumDistance) {
             if (this.canBeginWithTravelledDistance(new Point(location.x - this._translationOrigin.x, location.y - this._translationOrigin.y))) {
                 this._travelledMinimumDistance = true;
                 this.enterBeganState();
             }
-        } else {
-            this.enterChangedState();
-
-            var sliceIndexX = 0;
-            var sliceIndexY = 0;
-
-            var currentGesture = this._gestures[this._gestures.length - 1];
-            var previousGesture = this._gestures[this._gestures.length - 2];
-
-            var txDirection = currentGesture.location.x >= previousGesture.location.x;
-            var tyDirection = currentGesture.location.y >= previousGesture.location.y;
-            for (var i = this._gestures.length - 3; i >= 0; --i) {
-                var gesture = this._gestures[i];
-                if (currentTime - gesture.timeStamp > PanGestureRecognizer.MaximumTimeForRecordingGestures) {
-                    if (sliceIndexX === 0)
-                        sliceIndexX = i + 1;
-                    if (sliceIndexY === 0)
-                        sliceIndexY = i + 1;
-                    break;
-                }
-                
-                var nextTranslation = this._gestures[i + 1].location;
-                
-                if (nextTranslation.x >= gesture.location.x !== txDirection)
-                    sliceIndexX = i + 1;
-                if (nextTranslation.y >= gesture.location.y !== tyDirection)
-                    sliceIndexY = i + 1;
-                
-                if (sliceIndexX > 0 && sliceIndexY > 0) 
-                    break;
-            }
-
-            var xGestures = this._gestures,
-                yGestures = this._gestures;
-
-            if (sliceIndexX === sliceIndexY && sliceIndexX > 0)
-                this._gestures = this._gestures.slice(sliceIndexX);
-            else {
-                if (sliceIndexX > 0)
-                    xGestures = this._gestures.slice(sliceIndexX);
-                if (sliceIndexY > 0)
-                    yGestures = this._gestures.slice(sliceIndexY);
-            } 
-
-            var oldestGestureX = xGestures[0];
-            var xdt = currentTime - oldestGestureX.timeStamp;
-            var dx = location.x - oldestGestureX.location.x;
-            this._velocity.x = dx / xdt * 1000;
-
-            var oldestGestureY = yGestures[0];
-            var ydt = currentTime - oldestGestureY.timeStamp;
-            var dy = location.y - oldestGestureY.location.y;
-            this._velocity.y = dy / ydt * 1000;
-            
+        } else {            
             this.translation.x += location.x - previousGesture.location.x;
             this.translation.y += location.y - previousGesture.location.y;
+
+            this.enterChangedState();
         }
     },
 
